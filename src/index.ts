f4ad5f70da0c20a21f5fc9145f78c980749fc29e
Vilen Kupriienko <pride2808@gmail.com>
2021-03-04 09:18:08 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const helloWorld = async (duration: number) => {
+	console.log('Hello World!');
+
+	return new Promise((resolve) => {
+		setTimeout(resolve, duration);
+	});
+};
+
+helloWorld(5000);
