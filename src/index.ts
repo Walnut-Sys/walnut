f627190471631c9461d7622751d77ca2f6f24abb
MykhailKo <tuareg1812@gmail.com>
2021-03-19 21:54:26 +0200

    Reviews formating, added sonarlint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const helloWorld = async (duration: number) => {
+const helloWorld = async (duration: number): Promise<void> => {
 	console.log('Hello World!');
 	return new Promise((resolve) => {
 		setTimeout(resolve, duration);
