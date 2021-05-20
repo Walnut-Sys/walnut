6fb40b84b830d7c620ed0e3600e964db42c384cd
Vilen Kupriienko <pride2808@gmail.com>
2021-05-20 11:36:05 +0300

    JSON output composer was added

--- src/core/output-composers/json.ts
+++ src/core/output-composers/json.ts
@@ -1,9 +1,16 @@
 import IOutputComposer from '../interfaces/output-composer';
+import IParserOutput from '../interfaces/parser-output';
+import ComposingError from '../errors/composing-error';
 
 export default class JSONComposer implements IOutputComposer {
-  public async compose(): Promise<Buffer> {
-    return new Promise<Buffer>((resolve, reject) => {
-      resolve(Buffer.alloc(8));
-    });
+  public async compose(parserOutput: IParserOutput): Promise<Buffer> {
+    try {
+      return Buffer.from(
+        JSON.stringify(parserOutput, null, '\t')
+      );
+    }
+    catch (error) {
+      throw new ComposingError(`Error while composing JSON output: ${error}`);
+    }
   }
 }
