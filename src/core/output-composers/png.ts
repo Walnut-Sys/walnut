c306db061360010e3699d5e65815c5291434edb4
Vilen Kupriienko <pride2808@gmail.com>
2021-05-20 11:25:04 +0300

    PNG output composer was updated

--- src/core/output-composers/png.ts
+++ src/core/output-composers/png.ts
@@ -1,15 +1,15 @@
-import { Readable } from 'stream';
 import sharp from 'sharp';
 import IOutputComposer from '../interfaces/output-composer';
 import IParserOutput from '../interfaces/parser-output';
-import { fillSVGTemplate } from '../utils/svg';
 import ComposingError from '../errors/composing-error';
+import { fillSVGTemplate } from '../utils/svg';
+import { IMAGE_WIDTH, IMAGE_HEIGHT } from '../constants';
 
 export default class PNGComposer implements IOutputComposer {
   public async compose(parserOutput: IParserOutput): Promise<Buffer> {
     try {
       const svgBuffer = await fillSVGTemplate(parserOutput);
-      return sharp(svgBuffer).png().toBuffer();
+      return sharp(svgBuffer).png().resize(IMAGE_WIDTH, IMAGE_HEIGHT).toBuffer();
     }
     catch (error) {
       throw new ComposingError(`Error while composing png output: ${error}`);
