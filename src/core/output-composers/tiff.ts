c1b3b4b779fcee59af68f2bce514fb963ccb8936
Vilen Kupriienko <pride2808@gmail.com>
2021-05-20 11:40:22 +0300

    TIFF output composer was added

--- src/core/output-composers/tiff.ts
+++ src/core/output-composers/tiff.ts
@@ -1,9 +1,18 @@
+import sharp from 'sharp';
 import IOutputComposer from '../interfaces/output-composer';
+import IParserOutput from '../interfaces/parser-output';
+import ComposingError from '../errors/composing-error';
+import { fillSVGTemplate } from '../utils/svg';
+import { IMAGE_WIDTH, IMAGE_HEIGHT } from '../constants';
 
 export default class TIFFComposer implements IOutputComposer {
-  public async compose(): Promise<Buffer> {
-    return new Promise<Buffer>((resolve, reject) => {
-      resolve(Buffer.alloc(8));
-    });
+  public async compose(parserOutput: IParserOutput): Promise<Buffer> {
+    try {
+      const svgBuffer = await fillSVGTemplate(parserOutput);
+      return sharp(svgBuffer).tiff().resize(IMAGE_WIDTH, IMAGE_HEIGHT).toBuffer();
+    }
+    catch (error) {
+      throw new ComposingError(`Error while composing TIFF output: ${error}`);
+    }
   }
 }
