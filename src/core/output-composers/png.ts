7c2e992ee52b953083a5018871e08796e99b6fc0
MykhailKo <tuareg1812@gmail.com>
2021-05-20 08:37:08 +0300

    added toFormat in output composer

--- src/core/output-composers/png.ts
+++ src/core/output-composers/png.ts
@@ -9,7 +9,7 @@ export default class PNGComposer implements IOutputComposer {
   public async compose(parserOutput: IParserOutput): Promise<Readable> {
     const svgBuffer = await fillSVGTemplate(parserOutput);
     try {
-      const data = await sharp(svgBuffer).toBuffer();
+      const data = await sharp(svgBuffer).toFormat('png').toBuffer();
       return Readable.from([data]);
     } catch (error) {
       throw new ComposingError('Error while composing png output');
