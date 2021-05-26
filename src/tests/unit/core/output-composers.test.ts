c3819503a071ddd00258ca79c061360f31c5e9df
Vilen Kupriienko <pride2808@gmail.com>
2021-05-27 01:06:16 +0300

    Check sums for tests were changed

--- src/tests/unit/core/output-composers.test.ts
+++ src/tests/unit/core/output-composers.test.ts
@@ -10,6 +10,7 @@ describe('Output composers', () => {
     test('HTML composer with valid input', async () => {
       const expectedCheckSums: ExpectedCheckSums = {
         win32: 'c4b8aa5d80657d9c07d6abcf0e7b3676',
+        darwin: 'eb7fd023cfdc891ca5e117b37c696015',
         linux: 'eb7fd023cfdc891ca5e117b37c696015'
       };
 
@@ -25,15 +26,14 @@ describe('Output composers', () => {
     test('PNG composer with valid input', async () => {
       const expectedCheckSums: ExpectedCheckSums = {
         win32: 'ce3d4255fcf19e21ec3af78cba76df0c',
-        linux: '10d5c5b3ae92308873a5da3b7df6541e'
+        darwin: '4318bd56b45260a349797b16dbb49aa8',
+        linux: '3ab0a9c8357caa01c2d81101957291c1'
       };
 
       const pngComposer = new composers.PNGComposer();
       const pngBuffer = await pngComposer.compose(parserOutputMock);
 
-      const receivedCheckSum = generateCheckSum(pngBuffer.toString(), {
-        inputEncoding: 'binary'
-      });
+      const receivedCheckSum = generateCheckSum(pngBuffer.toString());
       const expectedCheckSum = expectedCheckSums[os.platform()] || expectedCheckSums.linux;
       expect(receivedCheckSum).toEqual(expectedCheckSum);
     });
@@ -42,15 +42,14 @@ describe('Output composers', () => {
     test('JPEG composer with valid input', async () => {
       const expectedCheckSums: ExpectedCheckSums = {
         win32: '8bbec4bfdee9aa2a9749cc1eb60a2803',
-        linux: '6e625727177a48e2788a45dfb8691255'
+        darwin: '0978e66c3543423e9f722aa7d6925b30',
+        linux: '92337e7074b55c5075e5330e2e260fe7'
       };
 
       const jpegComposer = new composers.JPEGComposer();
       const jpegBuffer = await jpegComposer.compose(parserOutputMock);
 
-      const receivedCheckSum = generateCheckSum(jpegBuffer.toString(), {
-        inputEncoding: 'binary'
-      });
+      const receivedCheckSum = generateCheckSum(jpegBuffer.toString());
       const expectedCheckSum = expectedCheckSums[os.platform()] || expectedCheckSums.linux;
       expect(receivedCheckSum).toEqual(expectedCheckSum);
     });
@@ -59,15 +58,14 @@ describe('Output composers', () => {
     test('WEBP composer with valid input', async () => {
       const expectedCheckSums: ExpectedCheckSums = {
         win32: 'eb51044567e7cb9a17cd446cc22959a0',
-        linux: '76b27fc080154e510856042553675914'
+        darwin: '1f92324554b7fd224816ab9094636c5e',
+        linux: '15751710471d7a13df07e6c85ac1f2e1'
       };
 
       const webpComposer = new composers.WEBPComposer();
       const webpBuffer = await webpComposer.compose(parserOutputMock);
 
-      const receivedCheckSum = generateCheckSum(webpBuffer.toString(), {
-        inputEncoding: 'binary'
-      });
+      const receivedCheckSum = generateCheckSum(webpBuffer.toString());
       const expectedCheckSum = expectedCheckSums[os.platform()] || expectedCheckSums.linux;
       expect(receivedCheckSum).toEqual(expectedCheckSum);
     });
@@ -76,15 +74,14 @@ describe('Output composers', () => {
     test('TIFF composer with valid input', async () => {
       const expectedCheckSums: ExpectedCheckSums = {
         win32: '7f95ef59c470ec7804ac658496d84af0',
-        linux: 'edf52fd8ce672b4b51ebfe542f6c24a7'
+        darwin: 'dcef00e844cc24d6b8d793f098d55bc7',
+        linux: '5302104bd19f3323798c762dd4aa5a21'
       };
 
       const tiffComposer = new composers.TIFFComposer();
       const tiffBuffer = await tiffComposer.compose(parserOutputMock);
 
-      const receivedCheckSum = generateCheckSum(tiffBuffer.toString(), {
-        inputEncoding: 'binary'
-      });
+      const receivedCheckSum = generateCheckSum(tiffBuffer.toString());
       const expectedCheckSum = expectedCheckSums[os.platform()] || expectedCheckSums.linux;
       expect(receivedCheckSum).toEqual(expectedCheckSum);
     });
@@ -93,6 +90,7 @@ describe('Output composers', () => {
     test('JSON composer with valid input', async () => {
       const expectedCheckSums: ExpectedCheckSums = {
         win32: 'ac8f95a61c3db79280b3c1179f430993',
+        darwin: 'ac8f95a61c3db79280b3c1179f430993',
         linux: 'ac8f95a61c3db79280b3c1179f430993'
       };
 
@@ -108,6 +106,7 @@ describe('Output composers', () => {
     test('XML composer with valid input', async () => {
       const expectedCheckSums: ExpectedCheckSums = {
         win32: 'e6a45a7fac5725b2a493bfe0c4e51207',
+        darwin: '5a18a14220323e8b5e2af5b7370a3e18',
         linux: '5a18a14220323e8b5e2af5b7370a3e18'
       };
 
