6a0c2c10dd55114094f5a0eb8617eb7ffb435cc0
Vilen Kupriienko <pride2808@gmail.com>
2021-05-29 17:49:36 +0300

    Some code was formatted

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -9,7 +9,7 @@ import crypto from 'crypto';
  * @return {any}
  */
 export const standardizeValue = (value: any, supportedValues: any[], defaultValue: any) => {
-  const supportedValue = supportedValues.find(supportedValue => {
+  const supportedValue = supportedValues.find((supportedValue) => {
     if (typeof value === 'string') {
       return supportedValue.toLowerCase() === value.toLowerCase();
     }
@@ -20,13 +20,13 @@ export const standardizeValue = (value: any, supportedValues: any[], defaultValu
   return supportedValue ?? defaultValue;
 };
 
-export const generateCheckSum = (data: any, {
-  algorithm = 'md5',
-  inputEncoding = 'utf-8' as crypto.Encoding,
-  outputEncoding = 'hex' as crypto.BinaryToTextEncoding
-} = {}): string => {
-  return crypto
-    .createHash(algorithm)
-    .update(data, inputEncoding)
-    .digest(outputEncoding);
+export const generateCheckSum = (
+  data: any,
+  {
+    algorithm = 'md5',
+    inputEncoding = 'utf-8' as crypto.Encoding,
+    outputEncoding = 'hex' as crypto.BinaryToTextEncoding
+  } = {}
+): string => {
+  return crypto.createHash(algorithm).update(data, inputEncoding).digest(outputEncoding);
 };
